f3b4eebc872a31002855b0c3f281c801b5d5aee6
Adarshtheki <adarshverma549@gmail.com>
2025-06-22 10:26:52 +0530

    update socket.io feature

--- server/src/socket.js
+++ server/src/socket.js
@@ -1,7 +1,11 @@
-import { ChatEvents } from "./utils/chatEvents";
+import { User } from "./models/user.model.js";
+import { ApiError } from "./utils/ApiError.js";
+import { ChatEvents } from "./utils/chatEvents.js";
+import jwt from "jsonwebtoken";
 
 const mountJoinChatEvent = (socket) => {
   socket.on(ChatEvents.JOIN_CHAT_EVENT, (chatId) => {
+    console.log(`User joined the chat 🤝. chatId: `, chatId);
     socket.join(chatId);
   });
 };
@@ -21,7 +25,37 @@ const mountParticipantStoppedTypingEvent = (socket) => {
 const initializeSocketIO = (io) => {
   return io.on("connection", async (socket) => {
     try {
-      //
+      const token =
+        socket?.cookies?.accessToken ||
+        socket.headers.authorization?.split(" ")[1];
+
+      if (!token) throw new ApiError(401, "Unauthorized, Token not found");
+
+      const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+
+      const user = await User.findById(decoded._id).select("-password");
+
+      if (!user) {
+        throw new ApiError(401, "Un-authorized handshake. Token is invalid");
+      }
+
+      socket.user = user;
+
+      socket.join(user._id.toString());
+      socket.emit(ChatEvents.CONNECTED_EVENT); // emit the connected event so that client is aware
+      console.log("User connected 🗼. userId: ", user._id.toString());
+
+      // Common events that needs to be mounted on the initialization
+      mountJoinChatEvent(socket);
+      mountParticipantTypingEvent(socket);
+      mountParticipantStoppedTypingEvent(socket);
+
+      socket.on(ChatEvents.DISCONNECT_EVENT, () => {
+        console.log("user has disconnected 🚫. userId: " + socket.user?._id);
+        if (socket.user?._id) {
+          socket.leave(socket.user._id);
+        }
+      });
     } catch (error) {
       socket.emit(
         ChatEvents.SOCKET_ERROR_EVENT,
@@ -30,3 +64,9 @@ const initializeSocketIO = (io) => {
     }
   });
 };
+
+const emitSocketEvent = (req, roomId, event, payload) => {
+  req.app.get("io").to(roomId).emit(event, payload);
+};
+
+export { emitSocketEvent, initializeSocketIO };
