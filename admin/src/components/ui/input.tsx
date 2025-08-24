4aecd741e82d39b326f93c55130b9feb6f48da46
Adarshtheki <adarshverma549@gmail.com>
2025-08-24 23:43:36 +0530

    fixed input with shadecn component

--- admin/src/components/ui/input.tsx
+++ admin/src/components/ui/input.tsx
@@ -1,35 +1,31 @@
-import React, { useId } from 'react';
+import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
-    const id = useId();
-    return (
-      <div className="flex flex-col gap-0.5 w-full">
-        {props.name && (
-          <label
-            htmlFor={props.name || id}
-            className="capitalize text-sm font-medium text-gray-700">
-            {props.name}
-          </label>
-        )}
-        <input
-          id={props.name || id}
-          placeholder={cn('Please enter', props.name)}
-          type={type}
-          className={cn(
-            'flex h-10 w-full rounded-md border py-2 px-4 border-gray-400',
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
-    );
-  }
-);
+function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
+  const id = React.useId();
 
-Input.displayName = 'Input';
+  return (
+    <div className="flex flex-col gap-0.5 w-full">
+      {props.name && (
+        <label
+          htmlFor={props.name || id}
+          className="capitalize text-sm font-medium text-gray-700">
+          {props.name}
+        </label>
+      )}
+      <input
+        id={props.name || id}
+        placeholder={cn('Please enter', props.name)}
+        type={type}
+        className={cn(
+          'flex h-10 w-full rounded-md border py-2 px-4 border-gray-400',
+          className
+        )}
+        {...props}
+      />
+    </div>
+  );
+}
 
 export default Input;
